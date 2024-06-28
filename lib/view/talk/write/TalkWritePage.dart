2723b3b122e213f0aa0cd07427dd99bbc1adebaf
leesehwi <leesehwi@minigate.net>
2024-06-28 12:29:11 +0900

    사진 업로드 프론트

--- lib/view/talk/write/TalkWritePage.dart
+++ lib/view/talk/write/TalkWritePage.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:modu_flutter/apis/Talk/TalkApi.dart';
 import 'package:modu_flutter/apis/Talk/TalkModel.dart';
 import 'package:modu_flutter/main.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 import '../../../provider/TalkStore.dart';
@@ -15,6 +19,69 @@ class TalkWritePage extends StatefulWidget {
 }
 
 class _BoardWriteState extends State<TalkWritePage> {
+  XFile? _image; // 이미지를 담을 변수 선언
+  final ImagePicker _picker = ImagePicker();
+  setXfile(image) {
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future<void> _requestPermission() async {
+    var status = await Permission.photos.status;
+    if (!status.isGranted) {
+      status = await Permission.photos.request();
+    }
+
+    if (status.isGranted) {
+      print("사진첩 접근 권한 허용됨");
+      _pickImage(); // 권한이 허용된 경우 이미지 선택 함수 호출
+    } else if (status.isDenied) {
+      print("사진첩 접근 권한 거부됨");
+    } else if (status.isPermanentlyDenied) {
+      print("사진첩 접근 권한 영구적으로 거부됨");
+      _showPermissionDialog();
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = pickedFile; // 선택된 파일을 상태로 저장
+      });
+      print("선택된 파일 경로: ${pickedFile.path}");
+    } else {
+      print("이미지 선택 취소됨");
+    }
+  }
+
+  void _showPermissionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) =>
+          AlertDialog(
+            title: Text('권한 필요'),
+            content: Text('사진첩 접근 권한이 필요합니다. 설정에서 권한을 허용해주세요.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('취소'),
+              ),
+              TextButton(
+                onPressed: () {
+                  openAppSettings();
+                  Navigator.of(context).pop();
+                },
+                child: Text('설정으로 이동'),
+              ),
+            ],
+          ),
+    );
+  }
+
   var talkCont;
 
   insetTalk() async {
@@ -24,13 +91,13 @@ class _BoardWriteState extends State<TalkWritePage> {
         action: SnackBarAction(
           label: '확인',
           onPressed: () {
-            // 확인 버튼 클릭시 실행할거
+            // 확인 버튼 클릭시 실행할 것
           },
         ),
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
     } else {
-      Talk talkInput = new Talk();
+      Talk talkInput = Talk();
       talkInput.talkCont = talkCont;
 
       await TalkApi.insertTalk(talkInput.toJson());
@@ -57,60 +124,92 @@ class _BoardWriteState extends State<TalkWritePage> {
               icon: Icon(Icons.check))
         ],
       ),
-      body: Container(
-        child: Column(
-          children: [
-            // TODO: 토크 입력 TEXTFIELD
-            Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: _controller,
-                    maxLength: 200,
-                    maxLines: 8,
-                    // or null
-                    decoration: InputDecoration.collapsed(
-                      hintText: "내용 입력",
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              // TODO: 토크 입력 TEXTFIELD
+              Card(
+                  color: Colors.grey,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _controller,
+                      maxLength: 200,
+                      maxLines: 8,
+                      // or null
+                      decoration: InputDecoration.collapsed(
+                        hintText: "내용 입력",
+                      ),
+                      inputFormatters: [
+                        _LengthLimitingTextInputFormatterFixed(200),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          talkCont = value;
+                        });
+                      },
                     ),
-                    inputFormatters: [
-                      _LengthLimitingTextInputFormatterFixed(200),
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        talkCont = value;
-                      });
-                    },
-                  ),
-                )),
-
-            // TODO: 토크 내용 전체 삭제 버튼
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton(
-                    onPressed: () {
-                      _controller.clear();
-                      setState(() {
-                        talkCont = "";
-                      });
-                    },
-                    child: Text("전체삭제")),
-              ],
-            ),
-
-            // TODO: 제제 대상 박스
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.black,
-                width: 2,
-              )),
-              child: Text("<제제대상> 어쩌구 저쩌구"),
-            )
-          ],
+                  )),
+        
+              // TODO: 토크 내용 전체 삭제 버튼
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        _controller.clear();
+                        setState(() {
+                          talkCont = "";
+                        });
+                      },
+                      child: Text("전체삭제")),
+                ],
+              ),
+        
+              _image == null
+                  ?
+              // TODO: 사진 등록
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.add_to_photos),
+                  onPressed: () {
+                    _requestPermission();
+                  },
+                  iconSize: 70,
+                ),
+              )
+                  : Column(children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  child:
+                  Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
+                ),
+                Container(
+                    child: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        setXfile(null);
+                      },
+                      iconSize: 70,
+                    ),
+                )
+              ]),
+        
+              // TODO: 제제 대상 박스
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    )),
+                child: Text("<제제대상> 어쩌구 저쩌구"),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -124,8 +223,8 @@ class _LengthLimitingTextInputFormatterFixed extends TextInputFormatter {
   _LengthLimitingTextInputFormatterFixed(this.maxLength);
 
   @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue,
+      TextEditingValue newValue) {
     if (newValue.text.characters.length > maxLength) {
       return oldValue;
     }
